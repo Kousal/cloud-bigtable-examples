d11e981a33b7c479e6e9e8f6c04c5675e33035f1
Kousal <45628479+Kousal@users.noreply.github.com>
2023-09-11 19:40:28 +0530

    Update WordCountDriver.java

--- java/dataproc-wordcount/src/main/java/com/example/bigtable/sample/WordCountDriver.java
+++ java/dataproc-wordcount/src/main/java/com/example/bigtable/sample/WordCountDriver.java
@@ -1,43 +1,64 @@
-/**
- * Copyright 2015 Google Inc. All Rights Reserved.
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.example.bigtable.sample;
 
-import org.apache.hadoop.hbase.mapreduce.Export;
-import org.apache.hadoop.util.ProgramDriver;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class WordCountDriver {
 
-  public static void main(String[] args) {
-    ProgramDriver programDriver = new ProgramDriver();
-    int exitCode = -1;
-    try {
-      programDriver.addClass("wordcount-hbase", WordCountHBase.class,
-          "A map/reduce program that counts the words in the input files.");
-      programDriver.addClass("export-table", Export.class,
-          "A map/reduce program that exports a table to a file.");
-      //programDriver.addClass("cellcounter", CellCounter.class, "Count them cells!");
-      programDriver.driver(args);
-      exitCode = programDriver.run(args);
-    } catch (Throwable e) {
-      e.printStackTrace();
+  public static class TokenizerMapper
+          extends Mapper<Object, Text, Text, IntWritable>{
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context
+    ) throws IOException, InterruptedException {
+      StringTokenizer itr = new StringTokenizer(value.toString());
+      while (itr.hasMoreTokens()) {
+        word.set(itr.nextToken());
+        context.write(word, one);
+      }
     }
-    System.exit(exitCode);
+  }
+
+  public static class IntSumReducer
+          extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      System.out.print(key + "-" + sum);
+      context.write(key, result);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "word count");
+    job.setJarByClass(WordCountDriver.class);
+    job.setMapperClass(TokenizerMapper.class);
+    //job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setNumReduceTasks(1);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
 }
